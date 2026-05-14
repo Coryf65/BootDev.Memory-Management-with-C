ae04a446cfac6e52943607cbec7ebca6cce368c4
Coryf65 <Coryf65@users.noreply.github.com>
2026-05-13 20:12:58 -0500

    we can also swap struct, ints, and strings using a void*

--- swap_generic.c
+++ swap_generic.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+// swap different data between all sorts of data types
 void swap(void *vp1, void *vp2, size_t size) {
 
     printf("swapping!!\n");
@@ -15,15 +15,63 @@ void swap(void *vp1, void *vp2, size_t size) {
     memcpy(temp, vp1, size);
 
     // a = b
-    memcpy(temp, vp1, size);
+    memcpy(vp1, vp2, size);
+
+    // b = temp
+    memcpy(vp2, temp, size);
+
+    // use free due to the use of malloc
+    free(temp);
 }
 
 int main()
 {
-    printf("when we do not know the sizes and types we can get we then do it dynamically / on the heap / malloc\n");
+    printf("when we do not know the sizes and types we can get we then do it dynamically / on the heap / malloc\n\n");
 
+    int i1 = 1234;
+    int i2 = 5678;
+    
+    printf("works for ints like...\n");
+    printf("l1 : %d\n", i1);
+    printf("i2 : %d\n", i2);
+    
+    swap(&i1, &i2, sizeof(int));
 
+    if (i1 != 5678)
+    {
+        printf("i1 should be i2's original value\n");
+    }
 
+    if (i2 != 1234)
+    {
+        printf("i2 should be i1's original value\n");
+    }
+
+    printf("now l1 : %d\n", i1);
+    printf("now i2 : %d\n", i2);
+
+    printf("\nThis also works with structs or strings\n");
+
+    char *s1 = "dax";
+    char *s2 = "adam";
+
+    printf("s1 : %s\n", s1);
+    printf("s2 : %s\n", s2);
+
+    swap(&s1, &s2, sizeof(char *));
+    
+    if (s1 != "adam")
+    {
+        printf("s1 should be s2's original value\n");
+    }
+
+    if (s2 != "dax")
+    {
+        printf("s2 should be s1's original value\n");
+    }
+
+    printf("now s1 : %s\n", s1);
+    printf("now s2 : %s\n", s2);
 
     return 0;
 }
